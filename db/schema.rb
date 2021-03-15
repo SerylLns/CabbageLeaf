b16134241ebd49d95a18978c5507d86309891ab3
yanis khellaf <yanis.khellaf@outlook.fr>
2021-03-15 17:51:45 +0100

    add validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_15_153233) do
+ActiveRecord::Schema.define(version: 2021_03_15_161539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,9 +26,9 @@ ActiveRecord::Schema.define(version: 2021_03_15_153233) do
   end
 
   create_table "interactions", force: :cascade do |t|
-    t.boolean "has_read"
-    t.boolean "read_later"
-    t.boolean "liked"
+    t.boolean "has_read", default: false, null: false
+    t.boolean "read_later", default: false, null: false
+    t.boolean "liked", default: false, null: false
     t.bigint "user_id", null: false
     t.bigint "article_id", null: false
     t.datetime "created_at", precision: 6, null: false
