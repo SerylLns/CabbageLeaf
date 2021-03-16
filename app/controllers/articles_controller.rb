c0508a4770c195e3a8e2b7974adc0f2cab403112
seryl lounis <seryllounis@outlook.fr>
2021-03-16 17:57:09 +0100

    Fucking seed finish with success

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,7 @@
 class ArticlesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :index, :show, :rank ]
+  skip_before_action :authenticate_user!, only: [ :index]
   def index
     @articles = Article.all
-
   end
 
   def show
