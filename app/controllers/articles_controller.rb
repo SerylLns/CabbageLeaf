1f25b6f139dca97293b2470bf8d7cb267f57787f
yanis khellaf <yanis.khellaf@outlook.fr>
2021-03-17 17:21:05 +0100

    je suis perdu

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,12 @@
 class ArticlesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index]
-  
+
   def index
     @articles = Article.all
   end
 
   def show
-    @artcile = Article.find(params[:id])
+    @article = Article.find(params[:id])
   end
 
   def rank
