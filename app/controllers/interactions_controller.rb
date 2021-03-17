f737cf78941a6ba9204ed6cce2a38e6cc0e2fdf3
seryl lounis <seryllounis@outlook.fr>
2021-03-17 21:29:49 +0100

    Stating button of index

--- app/controllers/interactions_controller.rb
+++ app/controllers/interactions_controller.rb
@@ -4,8 +4,21 @@ class InteractionsController < ApplicationController
     @interaction = Interaction.new
   end
 
+  def create
+    article = Article.find(params[:article_id])
+    interaction = Interaction.new
+    interaction.user = current_user
+    interaction.article = article
+    interaction.save!
+    redirect_to articles_path
+  end
+
   def show
     @interaction = Interaction.find(params[:id])
     redirect_to article_path(@interaction.article)
+  end
+
+  def interaction_params
+    params.require(:interaction).permit()
   end
 end
