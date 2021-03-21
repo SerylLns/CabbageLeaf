b22c30128dbe9b8984208ff7fd35564c16b58f83
seryl lounis <seryllounis@outlook.fr>
2021-03-21 14:05:00 +0100

    must talk about submit btn

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,9 @@ class PagesController < ApplicationController
   def account
     @articles = current_user.articles.order(interactions: :desc)
   end
+  def filter
+    categorie = params[:categorie]
+    @articles = current_user.articles.select {|article| article.get_tags.include?(categorie)}
+    render :account
+  end
 end
